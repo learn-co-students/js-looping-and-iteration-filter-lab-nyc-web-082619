6eb62273e790625ddb675cd7507c3ba48895e428
Tania Manco <belalov.tania16@gmail.com>
2019-10-05 21:54:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file
+function findMatching(list, name) {
+    return list.filter(item => item.toLowerCase() == name.toLowerCase());
+}
+ function fuzzyMatch(list, name) {
+     return list.filter(item => item[0] == name[0] )
+ }
+
+ function matchName(list, name){
+    return list.filter(item => item.name == name);
+ }
